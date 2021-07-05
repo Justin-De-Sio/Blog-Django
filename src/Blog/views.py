129a68eb021d03a53b53d623335393777e7438f5
Justin-De-Sio <desio.j@live.fr>
2021-07-05 15:51:09 +0200

    Initial commit

---
+++ src/Blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from datetime import datetime
+
+def index(request):
+    date = datetime.today()
+    return render(request, "index.html", context={"date": date})
---
+++ src/blogue/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BlogueConfig(AppConfig):
+    name = 'blogue'
