b5d5c63b910a3c4a5f521037a965d4d8e741897a
Justin-De-Sio <desio.j@live.fr>
2021-07-05 17:38:51 +0200

    ajout d'une vue pour plusieur article. ajout d'une condition qui vérifie que le numero d'article voulu existe

--- src/Blog/urls.py
+++ src/Blog/urls.py
@@ -20,6 +20,6 @@ from .views  import index
 
 urlpatterns = [
     path('', index, name="index"),
-    path('blog/', include("blogue.urls")), #lier les urls
+    path('blogue/', include("blogue.urls")), #lier les urls
     path('admin/', admin.site.urls),
 ]
--- src/blogue/views.py
+++ src/blogue/views.py
@@ -1,9 +1,20 @@
-
-
-
 # Create your views here.
 from django.shortcuts import render
 
 
+def get_id_articles():
+    from pathlib import Path
+    DIR = Path(__file__).parent.absolute() / "templates" / "blogue"
+    exception  = ["article_not_found", "index"]
+    return [f[-2:] for f in [f.stem for f in DIR.iterdir()] if f not in exception ]
+
+
 def index(request):
     return render(request, "blogue/index.html")
+
+
+def article(request, numero_article):
+    if numero_article in get_id_articles():
+        return render(request, f"blogue/article_{numero_article}.html")
+    else:
+        return render(request, "blogue/article_not_found.html")
